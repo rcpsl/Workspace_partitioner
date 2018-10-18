c21b6fc040a032c4bf217350b36ac33424c66545
Haitham Khedr <hkhedr@umd.edu>
2018-10-18 00:02:55 -0400

    Final commit

--- Code/v1/run.py
+++ Code/v1/run.py
@@ -5,53 +5,54 @@ import numpy as np
 def main():
     np.random.seed(0)
     cpu_cnt = multiprocessing.cpu_count()
-    print('CPU count: %d'%cpu_cnt)
 
     #PARAMS
 
     from_region = [1,2]
-    # to_region = 23
+    to_region = 23
     to_regions = [0,2,3,6,7,13,23]
+    # to_regions = [18,20,21,22,24]
+
     PREPROCESS = False
     USE_CTR_EX = False 
     max_iter = 30000
     verbosity = 'OFF'
-    n_layers = 3
-    start = 10
+    n_layers = 2
+    start = 80
     step  = 10
 
-    # layer_size = [start + step*i for i in range(4)]
-    # tasks_args = []
-    # for i in range(cpu_cnt -1):
-    #     layer_size = start + step*i
-    #     if(PREPROCESS):
-    #         fname = './experiments/exp_'+str(layer_size)+'_'+str(n_layers) +'_preprocess'
-    #     elif(USE_CTR_EX):
-    #         fname = './experiments/exp_'+str(layer_size)+'_'+str(n_layers) +'_CE'
-    #     else:
-    #         fname = './experiments/exp_'+str(layer_size)+'_'+str(n_layers)
- 
-
-    #     tasks_args.append((from_region[0], from_region[1],to_region,PREPROCESS, 
-    #                 USE_CTR_EX, max_iter,verbosity,n_layers,layer_size,fname))
-
     tasks_args = []
-    layer_size = 40
-    for i in range(cpu_cnt -1):
-        to_region = to_regions[i]
+    for i in range(cpu_cnt-2):
+        layer_size = start + step*i
         if(PREPROCESS):
-            fname = './experiments/3b/exp_'+str(to_region) +'_preprocess'
+            fname = './experiments/3a/preprocess/'+str(layer_size)+'_'+str(n_layers) +'_preprocess'
         elif(USE_CTR_EX):
-            fname = './experiments/3b/exp_'+str(to_region)+'_CE'
+            fname = './experiments/3a/'+str(layer_size)+'_'+str(n_layers) +'_CE'
         else:
-            fname = './experiments/3b/exp_'+str(to_region) 
+            fname = './experiments/3a/'+str(layer_size)+'_'+str(n_layers)
+ 
 
         tasks_args.append((from_region[0], from_region[1],to_region,PREPROCESS, 
                     USE_CTR_EX, max_iter,verbosity,n_layers,layer_size,fname))
 
+    # tasks_args = []
+    # layer_size = 40
+    # for i in range(cpu_cnt -1):
+    #     to_region = to_regions[i]
+    #     if(PREPROCESS):
+    #         fname = './experiments/3b/exp_'+str(to_region) +'_preprocess'
+    #     elif(USE_CTR_EX):
+    #         fname = './experiments/3b/exp_'+str(to_region)+'_CE'
+    #     else:
+    #         fname = './experiments/3b/exp_'+str(to_region) 
+
+    #     tasks_args.append((from_region[0], from_region[1],to_region,PREPROCESS, 
+    #                 USE_CTR_EX, max_iter,verbosity,n_layers,layer_size,fname))
+
 
     jobs = [multiprocessing.Process(target=f, args=args) for args in tasks_args]
-    
+    print('# of Jobs: %d'%len(jobs))
+
     
     for job in jobs:
         job.daemon = True
