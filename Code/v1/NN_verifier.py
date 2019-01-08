379f7ce7f5e4d893fc16a740765961daddd59e2a
Haitham Khedr <haithamkhedr@wireless-10-104-181-121.umd.edu>
2019-01-08 17:45:07 -0500

    Add scripts for table 4

--- Code/v1/NN_verifier.py
+++ Code/v1/NN_verifier.py
@@ -217,7 +217,10 @@ class NN_verifier:
                 print("=======Solution found========")
                 print 'Layers' + '\t' + 'hidden' + '\t' + 'neurons' + '\tTime'
                 print str(self.trained_nn.num_layers) + '\t' + str(self.trained_nn.hidden_layer_size) + '\t' + str(self.trained_nn.num_neurons) +'\t%.5f'%(end_time - start_time)
-
+            if(len(self.out_file) > 0):
+                f = open(self.out_file,'a+')
+                f.write('\t\t\t%.5f'%(end_time - start_time))
+                f.close()
 
     def __createVarMap(self, numOfRealVars,numOfIFVars):
         """
---
+++ Code/v1/table4/test15_CE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+H="10"  #Number of Neurons per hidden layer
+FROM_ABS_IDX="1"     #Starting Region abstract index
+FROM_REF_IDX="2" #Starting Regions refined index
+TO_ABS_IDX="2"       #End Region abstract index
+TO_REF_IDX="3"   #End Regions refined index
+PREPROCESS="0"              #Generate counter examples
+CTR_EX="1"    #Use Counter examples for the feasability problem, used when preprocess is 0 only
+NLAYERS="4"                 #Number of hidden layers + output layer
+VERBOSE="ON"              
